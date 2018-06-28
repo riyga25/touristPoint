650665f1d0a5d720fa4dd59de26002ef5b9c1d01
ole27988967 <ole27988967@yandex.ru>
2018-06-28 16:02:40 +0300

    Added buisnessman and addPlace pages

---
+++ src/pages/buisnessman.vue
@@ -0,0 +1,81 @@
+<template>
+    <main>
+      <v-layout>
+        <v-toolbar dark color="primary">
+          <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+          <v-toolbar-title>TouristPoint</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>more_vert</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-navigation-drawer v-model="drawer" temporary absolute width="200">
+          <v-list class="pt-0" dense>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title>Выход</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-navigation-drawer>
+      </v-layout>
+	  <router-link to="/addPlace">
+	      <v-btn class="btn btn-outline-success mr-3" block="true">Добавить место</v-btn> 
+	  </router-link>      
+    </main>
+</template>
+
+<script>    
+    
+    export default {
+	    name: 'buisnessman',
+        components: {            
+        },
+        data() {
+            return {
+                drawer: false
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    nav {
+        border-bottom: 1px solid #BDBDBD;
+    }
+
+    .filters-shop {
+        padding: 28px 0 21px 0;
+    }
+
+    .filters-shop__reset {
+        cursor: pointer;
+    }
+
+    .places {
+        display: flex;
+        height: 624px;
+        padding-bottom: 47px;
+    }
+
+    .places__map {
+      min-width: 100%;
+      min-height: 100%;
+      width: 100vw;
+      height: 100vh;
+    }
+
+    .places__scroll {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-width: 460px;
+        overflow-y: auto;
+        border-top: 1px solid #E0E0E0;
+        border-bottom: 1px solid #E0E0E0;
+    }
+
+    .places__not-found {
+        margin-top: 20px;
+    }
+</style>
--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,8 +1,12 @@
 import Home from '../pages/mainpage.vue';
+import buisnessman from '../pages/buisnessman.vue';
+import addPlace from '../pages/addPlace';
 import Auth from '../pages/Auth';
 
 
 export default [
     { path: '/', name: 'home', component: Home },
+	{ path: '/addPlace', name: 'addPlace', component: addPlace },
+	{ path: '/buisnessman', name: 'buisnessman', component: buisnessman },
     { path: '/auth', name: 'enter', component: Auth }
 ]
