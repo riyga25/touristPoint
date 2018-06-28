f9acfe383af64443b6a2e7cc501be38467a04dd1
riyga <riyga@mail.ru>
2018-06-29 01:55:02 +0400

    bugfix, add animation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <template>
 	<v-app id="app">
-    <router-view></router-view>
+    <transition :name="transitionType" mode="out-in">
+      <router-view></router-view>
+    </transition>
 	</v-app>
 </template>
 
 <script>
 	export default {
-		name: "App"
+		name: "App",
+    data: function () {
+      return{
+        transitionType : 'slide-left'
+      }
+    },
+    watch: {
+      '$route' (to, from) {
+        const toDepth = to.path.split('/').length;
+        const fromDepth = from.path.split('/').length;
+        this.transitionType = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+      }
+    }
 	}
 </script>
 
 <style lang="scss" scoped>
+  .slide-left-enter, .slide-right-leave-to {
+    -webkit-transform: translate(40px, 0);
+    transform: translate(40px, 0);
+    opacity: 0;
+  }
+  .slide-left-leave-to, .slide-right-enter {
+    -webkit-transform: translate(-40px, 0);
+    transform: translate(-40px, 0);
+    opacity: 0;
+  }
+  .slide-right-enter-active,
+  .slide-left-enter-active,
+  .slide-right-leave-active,
+  .slide-left-leave-active {
+    transition: all .3s ease-out;
+  }
 
+  .slide-fade-enter-active {
+    transition: all .6s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .6s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+    /* .slide-fade-leave-active below version 2.1.8 */ {
+    transform: translateX(50px);
+    opacity: 0;
+  }
 </style>
--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,14 +1,14 @@
 import Home from '../pages/mainpage.vue';
 import buisnessman from '../pages/buisnessman.vue';
 import addPlace from '../pages/addPlace';
-import Auth from '../pages/Auth';
+// import Auth from '../pages/Auth';
 import  SignIn from '../components/signin';
 import  SignUp from '../components/signup';
 
 export default [
     { path: '/', name: 'home', component: Home },
     { path: '/auth', name: 'enter', component: SignIn },
-    { path: '/registry', name: 'reg', component: SignUp }
+    { path: '/registry', name: 'reg', component: SignUp },
 	  { path: '/addPlace', name: 'addPlace', component: addPlace },
 	  { path: '/buisnessman', name: 'buisnessman', component: buisnessman },
 ]
